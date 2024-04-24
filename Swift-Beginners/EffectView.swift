bc64007f35bdabe572030272d73d3a709eab5321
sannma31 <kasaisyouun2004@gmail.com>
2024-04-25 02:08:59 +0900

    add:カメラの撮影、カメラの保存、画像のエフェクト途中

---
+++ Swift-Beginners/EffectView.swift
@@ -0,0 +1,18 @@
+//
+//  EffectView.swift
+//  Swift-Beginners
+//
+//  Created by 笠井翔雲 on 2024/04/25.
+//
+
+import SwiftUI
+
+struct EffectView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EffectView()
+}
